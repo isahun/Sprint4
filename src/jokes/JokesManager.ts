deaa41552a82312a22f22468610063a6bf645136
Irene <isahun7@gmail.com>
2026-01-22 13:32:38 +0100

    chore: add debut logs to jokes manager to check both APIs are being randomly used to pull jokes

--- src/jokes/JokesManager.ts
+++ src/jokes/JokesManager.ts
@@ -13,12 +13,23 @@ export class JokesManager {
     async getJoke(): Promise<string> { 
         const useDadApi = Math.random() > 0.5; //math.random generates decimal numbers between 0 and 1(not included), will be true 50% of the times
 
+        console.log('---- getJoke called ----');
+        console.log('Using Dad API', useDadApi);
+
         if (useDadApi) {
+            console.log('Calling DAD JOKE API');
+
             const data = await this.api.get<DadJokeResponse>({endpoint: DAD_JOKE_API.url, headers: DAD_JOKE_API.headers}); //telling TS: this call will return EXACTLY this (object = {id:string, joke:string, status:number})
+
+            console.log('Dad API response:', data)
 
             return data.joke;
         } else {
+            console.log('Calling CHUCK NORRIS API');
+
             const data = await this.api.get<ChuckJokeResponse>({endpoint: CHUCK_JOKE_API.url}); //telling TS: this call will return EXACTLY this (object = {value: string})
+
+            console.log('Chuck API response: ', data)
 
             return data.value;
         }
