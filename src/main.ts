8d49509d832568fa505e0791c0890becbb3ea499
Irene Virgínia <isahun7@gmail.com>
2026-01-22 18:29:01 +0100

    refactor: migrate project to Vite with Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 //Responsibility: Application entry point and event orchestration. WHEN data from jokesUI is painted. It initializes the app by selecting global DOM elements, instantiating UI components, and wiring user events. It contains NO business logic or rendering details. UI painting with data collected from other files
 
-import { JokesUI } from "./jokes/JokesUI";
+import { JokesUI } from './jokes/JokesUI';
 
 const jokeSpace = document.querySelector<HTMLParagraphElement>('#jokeSpace');
 const button = document.querySelector<HTMLButtonElement>('#nextJokeBtn');
