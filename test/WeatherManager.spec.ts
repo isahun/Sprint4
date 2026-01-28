e8188de019d0afcd98d25da8fe88d3aae9b2005c
Irene Virgínia <isahun7@gmail.com>
2026-01-28 22:11:17 +0100

    test: adapt WeatherManager test to weather_code and temperature response

--- test/WeatherManager.spec.ts
+++ test/WeatherManager.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, vi, it, test } from 'vitest'
+import { describe, expect, vi, it } from 'vitest'
 import { WeatherManager  } from '../src/weather/WeatherManager'
 
 
@@ -9,13 +9,14 @@ vi.mock('../src/services/ApiService', () => {
             get() {
                 return Promise.resolve({
                     current: {
-                        temperature_2m: 12.6
+                        temperature_2m: 12.6,
+                        weather_code: 0
                     }
-                })
+                });
             }
         }
-    }
-})
+    };
+});
 
 describe('WeatherManager', () => {
 
@@ -29,9 +30,12 @@ describe('WeatherManager', () => {
 
         //Assert: check result is the one expected
         //1. Check if it's a number
-        expect(typeof result).toBe('number')
+        expect(typeof result).toBe('number');
+        expect(result.current).toHaveProperty('temperature_2m');
+        expect(result.current).toHaveProperty('weather_code');
 
         //2. Check if it's THE number (12.6)
-        expect(result).toBe(12.6);
-    })
-})
+        expect(result.current.temperature_2m).toBe(12.6);
+        expect(result.current.weather_code).toBe(0);
+    });
+});
