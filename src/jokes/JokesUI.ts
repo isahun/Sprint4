365a6e0ecb983140b89f46d24c6139741d08e44b
Irene <isahun7@gmail.com>
2026-02-02 22:47:10 +0100

    ui(jokes): hide rating controls until a joke is displayed

--- src/jokes/JokesUI.ts
+++ src/jokes/JokesUI.ts
@@ -4,39 +4,101 @@ import { JokesManager } from "./JokesManager"; // .js necessary when not in a Vi
 
 export class JokesUI {
     private manager = new JokesManager();
+
     private jokeElement: HTMLParagraphElement;
     private button: HTMLButtonElement;
+    private reportElement: HTMLDivElement;
+    private scoreSelect: HTMLSelectElement;
+    private clearScoreBtn: HTMLButtonElement;
+    private showScoreBtn: HTMLButtonElement;
+    private scoreQuestion: HTMLLegendElement;
+
     private anyJoke = false;
 
-    constructor(jokeElement: HTMLParagraphElement, button: HTMLButtonElement) { //dom element that will update
+    constructor(jokeElement: HTMLParagraphElement, 
+        button: HTMLButtonElement, reportElement: HTMLDivElement, scoreSelect:HTMLSelectElement, clearScoreBtn: HTMLButtonElement, showScoreBtn: HTMLButtonElement,
+        scoreQuestion: HTMLLegendElement,
+    ) { //dom element that will update
         this.jokeElement = jokeElement;
         this.button = button;
+        this.reportElement = reportElement;
+        this.scoreSelect = scoreSelect;
+        this.clearScoreBtn = clearScoreBtn;
+        this.showScoreBtn = showScoreBtn;
+        this.scoreQuestion = scoreQuestion;
 
         this.button.textContent = "Hit me!"
+
+        this.hideBtns();
     }
 
-    async renderJoke() {
+    async renderJoke(): Promise<void> {
+        //Clear UI before loading new jk
+        this.clearReportUI();
+
         this.jokeElement.textContent = 'Loading...';
-        this.jokeElement.textContent = await this.manager.getJoke();
+        const joke = await this.manager.getJoke();
+        this.jokeElement.textContent = joke;
 
         if(!this.anyJoke) {
             this.button.textContent = "Next joke";
             this.anyJoke = true;
+            this.showBtns();
         }
     }
 
-    attachScoreSelect(scoreSelect: HTMLSelectElement, clearBtn:HTMLButtonElement) {
+    attachScoreSelect():void {
         //internal UI interactions --> score btns, translates clicks into num values, delivers to manager
-        scoreSelect.addEventListener('change', () => {
-            const value = Number(scoreSelect.value); 
-            if(!value) return; //in case we click an empty element
-
-            this.manager.rateCurrentJoke(value as 1 | 2 | 3);
+        this.scoreSelect.addEventListener("change", () => {
+            const value = this.scoreSelect.value;
+            
+            if(value === "1" || value === "2" || value === "3") {
+                this.manager.rateCurrentJoke(Number(value) as 1 | 2 | 3)
+            }
         });
 
-        clearBtn.addEventListener('click', () => {
-            scoreSelect.value = "";
-            this.manager.clearJoke();
+        this.clearScoreBtn.addEventListener('click', () => {
+            this.manager.clearCurrentScore();
+            this.scoreSelect.value = "";
+            this.reportElement.textContent = "Score successfully deleted."
         });
+
+        this.showScoreBtn.addEventListener('click', () => {
+            const report: string = this.renderReport();
+            this.reportElement.innerHTML = report;
+        });
+    }
+
+    renderReport():string {
+        const report = this.manager.showReport();
+        const emptyReport = (report.length === 0);
+
+        if (emptyReport) return `There are no rated jokes`;
+
+        const printReport = report.map((jkInput, index) =>
+        `<p>
+        ${index + 1}. ${jkInput.joke}<br>Score: ${jkInput.score}<br>Date: ${new Date(jkInput.date).toLocaleString()}
+        </p>`).join("");
+        
+        return printReport;
+    }
+
+    clearReportUI(): void {
+        this.scoreSelect.value="";
+        this.reportElement.textContent = "";
+    }
+
+    private hideBtns():void {
+        this.scoreSelect.style.display = "none";
+        this.clearScoreBtn.style.display = "none";
+        this.showScoreBtn.style.display = "none";
+        this.scoreQuestion.style.display = "none";
+    }
+
+    private showBtns():void {
+        this.scoreSelect.style.display = "inline-block";
+        this.clearScoreBtn.style.display = "inline-block";
+        this.showScoreBtn.style.display = "inline-block";
+        this.scoreQuestion.style.display = "inline-block";
     }
 }
